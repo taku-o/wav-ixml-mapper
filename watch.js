128b60701a4d4b2388ecd930272c5a29704cf447
taku.omi <taku.omi@gree.co.jp>
2019-07-02 18:21:00 +0900

    add template.

--- watch.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const fs = require('fs');
-fs.watch('work', {
-    persistent: true,
-    recursive: false,
-    encoding: 'utf8',
-}, (eventType, filename) => {
-    console.log(`${eventType}: ${filename}`);
-});
